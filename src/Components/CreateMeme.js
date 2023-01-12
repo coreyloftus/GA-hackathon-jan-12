40492dbc335746989af7afdf18e583d3d064f1e5
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-12 09:13:54 -0800

    Added React router to index.js. Created useState and useEffect for show page

--- src/Components/CreateMeme.js
+++ src/Components/CreateMeme.js
@@ -1,6 +1,16 @@
+// SHOW PAGE CREATE MEME PAGE
+import React from "react"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 import "../Styling/Components.css"
-const CreateMeme = (props) => {
 
+const CreateMeme = (props) => {
+    const [meme, setMeme] = useState();
+    const { id } = useParams()
+
+    useEffect(() => {
+
+    }, [])
     return (
         <div>
             <h2>Create Meme</h2>
