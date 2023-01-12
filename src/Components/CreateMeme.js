4e5824931364b0623d356318374153b720604db7
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-12 09:30:12 -0800

    Created get meme function in Show page

--- src/Components/CreateMeme.js
+++ src/Components/CreateMeme.js
@@ -4,10 +4,23 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import "../Styling/Components.css"
 
+//MEME COMPONENT
 const CreateMeme = (props) => {
     const [meme, setMeme] = useState("");
     const [editForm, setEditForm] = useState([])
     const { id } = useParams()
+
+    //GET MEME
+    const getMeme = async () => {
+        try{
+            const response = await fetch()
+            const foundMeme = await response.json();
+            setMeme(foundMeme)
+            setEditForm(foundMeme)
+        } catch (err){
+            console.error(err);
+        }
+    }
 
     useEffect(() => {
 
